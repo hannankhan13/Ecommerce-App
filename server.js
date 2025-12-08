8ed7e03c8719c431d25ac268a4cd28dbffd14433
Hannan Khan <hannan.khan098@gmail.com>
2025-12-08 21:02:52 +0530

    Add mongodb connection

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+// configure env
+dotenv.config();
+
+// database config
+connectDB();
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to E-commerce App",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+  );
+});
