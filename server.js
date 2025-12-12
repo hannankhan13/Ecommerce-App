160385f3b45a8b532d40291ee77d8d9e770d51a2
Hannan Khan <hannan.khan098@gmail.com>
2025-12-12 22:33:22 +0530

    Create create product api

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 
 // configure env
@@ -25,6 +26,7 @@ const PORT = process.env.PORT || 8080;
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 
 app.get("/", (req, res) => {
