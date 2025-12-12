f25797025b6438e067e174c3bc1ce2b269cdaae7
Hannan Khan <hannan.khan098@gmail.com>
2025-12-12 20:37:10 +0530

    Add category CRUD apis

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from './routes/categoryRoutes.js';
 import cors from 'cors';
 
 // configure env
@@ -22,7 +23,9 @@ app.use(morgan("dev"));
 const PORT = process.env.PORT || 8080;
 
 // routes
-app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+
 
 app.get("/", (req, res) => {
   res.send({
