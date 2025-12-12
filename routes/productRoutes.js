965a6b3ef055889adbea778ec23a05c0da2ea152
Hannan Khan <hannan.khan098@gmail.com>
2025-12-12 23:16:19 +0530

    Add get product photo route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
-import { createProductController, getProductController, getSingleProductController } from '../controllers/productController.js';
+import { createProductController, getProductController, getSingleProductController, productPhotoController } from '../controllers/productController.js';
 import formidable from 'express-formidable';
 
 const router = express.Router();
@@ -14,5 +14,9 @@ router.get("/get-product", getProductController);
 
 // single product
 router.get("/get-product/:slug", getSingleProductController);
+
+// get photo
+
+router.get("/product-photo/:pid", productPhotoController);
 
 export default router;
