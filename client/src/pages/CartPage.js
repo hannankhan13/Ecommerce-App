29e22fbfbd960f1418d54dc848edd91966a4095b
Hannan Khan <hannan.khan098@gmail.com>
2025-12-24 22:32:25 +0530

    Add changes to cart page

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -73,6 +73,32 @@ const CartPage = () => {
                         <p>Total | Checkout | Payment</p>
                         <hr />
                         <h4>Total : {totalPrice()}</h4>
+                        {auth?.user?.address ? (
+                            <>
+                                <div className="mb-3">
+                                    <h4>Current Address</h4>
+                                    <h5>{auth?.user?.address}</h5>
+                                    <button onClick={() => navigate('/dashboard/user/profile')} className='btn btn-outline-warning'>Update address</button>
+                                </div>
+                            </>
+                        ) : (
+                            <div className="mb-3">
+                                {
+                                    auth?.token ? (
+                                        <button
+                                            onClick={() => navigate('/dashboard/user/profile')}
+                                            className='btn btn-outline-warning'>Update address</button>
+                                    ) : (
+                                        <button
+                                            onClick={() => navigate('/login', {
+                                                state: "/cart"
+                                            })}
+                                            className='btn btn-outline-warning'
+                                        >Please login to checkout</button>
+                                    )
+                                }
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
